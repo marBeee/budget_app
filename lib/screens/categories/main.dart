c1937a09932c8422a4eec4de1298bd16466c002f
marBeee <marvi.fetiluna.20@usjr.edu.ph>
2021-02-05 16:35:17 +0800

    Update main.dart

--- lib/screens/categories/main.dart
+++ lib/screens/categories/main.dart
@@ -6,8 +6,32 @@ class Categories extends StatefulWidget {
 }
 
 class _CategoriesState extends State<Categories> {
+  int selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text('hello'));
+    return SafeArea(
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: (int _index) {
+            setState(() {
+              selectedIndex = _index;
+            });
+          },
+          currentIndex: selectedIndex,
+          items: [
+            BottomNavigationBarItem(
+                label: 'Chart', icon: Icon(Icons.bar_chart_rounded)),
+            BottomNavigationBarItem(
+                label: 'History', icon: Icon(Icons.history_edu))
+          ],
+        ),
+      ),
+    );
   }
 }
