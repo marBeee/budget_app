c3c41cff03c3f60a2241db7cf97ac9195434c0a4
marBeee <marvi.fetiluna.20@usjr.edu.ph>
2021-02-06 08:32:17 +0800

    added dummy data for category

---
+++ lib/screens/categories/dummyData.dart
@@ -0,0 +1,7 @@
+import 'package:budget_app/models/category.dart';
+
+List<Category> dummyCategories = [
+  Category(catId: 0, name: 'Food', maxAmount: 100, spentAmount: 50),
+  Category(catId: 0, name: 'Furniture', maxAmount: 200, spentAmount: 75),
+  Category(catId: 0, name: 'School', maxAmount: 300, spentAmount: 100),
+];
